88d26712e583049129ad91d7d500060668fd20f5
tasfia.sharmin1994@gmail.com <tasfia.sharmin1994@gmail.com>
2017-09-14 12:22:51 +0600

    initial

---
+++ src/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-ons-page>
+    <p style="text-align: center">
+      Welcome home.
+    </p>
+    <div id="map"></div>
+  </v-ons-page>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    mounted: function() {
+        this.initMap();
+},
+    methods: {
+        initMap: function() {
+            var map;
+            map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 83.9207, lng: 35.9565},
+            scrollwheel: false,
+            zoom: 14
+            })
+        }
+    }
+}
+</script>
+
+<style>
+#map 
+{height:300px;width:500px;}
+</style>
+
